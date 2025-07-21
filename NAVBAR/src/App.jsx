8ade84f5d663d1409d8e8be03094c5546e9301a7
Rakesh Kumar Mohanty <mohantyrakesh802@gmail.com>
2025-07-21 23:42:32 +0530

    hriii

---
+++ NAVBAR/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+import NavBar from './Components/NavBar/NavBarpractice'
+
+const App = () => {
+
+  const curr_theme = localStorage.getItem('curr_theme');
+  const [theme,setTheme] = useState(curr_theme ? curr_theme : 'light')
+
+  useEffect (() => {
+    localStorage.setItem('curr_theme', theme)
+  },[theme])
+  return (
+    <div className = {`container ${theme}`}>
+      <NavBar theme={theme} setTheme={setTheme} />
+    </div>
+  )
+}
+
+export default App
